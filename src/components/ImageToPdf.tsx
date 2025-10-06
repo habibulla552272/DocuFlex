223c06bb0cb885cce9c15d0f31f6de137d740d7b
Md.Habibulla <mdhabibulla146425@gmail.com>
2025-10-06 08:22:40 +0600

    image added

--- src/components/ImageToPdf.tsx
+++ src/components/ImageToPdf.tsx
@@ -1,18 +1,46 @@
 'use client'
-import React from 'react'
+import React, { useState } from 'react'
 import { jsPDF } from 'jspdf'
 const ImageToPdf = () => {
+    const [url, setUrl] = useState<string | null>(null)
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const file = e.target.files?.[0];
 
-
+        console.log('1', e.target.files?.[0])
+        console.log('2', file)
+        if (file) {
+            const reader = new FileReader();
+            console.log('3', reader);
+            reader.onload = (event) => {
+                setUrl(event.target?.result as string);
+            };
+            reader.readAsDataURL(file);
+        }
+        setUrl(e.target.value)
+        console.log('3', e.target.value)
+    }
     const handlepdf = () => {
-        const doc = new jsPDF();
+        if (!url) {
+            alert('Please Select an Image First');
+            return;
+        }
+        const doc = new jsPDF('p', 'mm', 'a4');
         doc.text("Hello worl", 10, 10);
+        const pageWidth = doc.internal.pageSize.getWidth();
+        const pageHeight = doc.internal.pageSize.getHeight();
+        doc.addImage(url, 'JPEG', 0, 0, pageWidth, pageHeight)
         doc.save('Example.pdf');
     }
     return (
-        <div>
-            <button onClick={() => handlepdf()} className='bg-green-400 px-10 py-3'>Click Here</button>
-        </div>
+        <section>
+            <div className='container mx-auto'>
+                <div>
+                    <h2>Chooce Your Image</h2>
+                    <input onChange={handleChange} type="file" placeholder='..' />
+                </div>
+                <button onClick={() => handlepdf()} className='bg-green-400 px-10 py-3 rounded-2xl cursor-pointer'>Click Here</button>
+            </div>
+        </section>
     )
 }
 
